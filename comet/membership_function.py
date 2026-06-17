f84ea27dabde8f959d1feb8ed7c55e3b387a6f4f
Charlys Augusto <charlysaugusto@prottus.com.br>
2026-06-17 00:22:51 -0300

    Modify membership function

--- comet/membership_function.py
+++ comet/membership_function.py
@@ -1,10 +1,13 @@
-def triangular_membership(x: float, a: float, b: float, c: float) -> float:
-
+def validate_triangular_params(a: float, b: float, c: float) -> None:
     if not (a <= b <= c):
         raise ValueError("Invalid parameters: a <= b <= c must hold.")
 
     if a == b == c:
         raise ValueError("Invalid parameters: a, b, and c can't all be equal.")
+
+
+def triangular_membership(x: float, a: float, b: float, c: float) -> float:
+    validate_triangular_params(a, b, c)
 
     if a == b: 
         if x <= a:
